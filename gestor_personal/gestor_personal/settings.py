061dfd1e5c3a1ff2beecf380909d5e6df3f8618a
ramic0 <65687960+ramic0@users.noreply.github.com>
2023-04-12 23:07:16 -0500

    Update settings.py

--- gestor_personal/gestor_personal/settings.py
+++ gestor_personal/gestor_personal/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contact',
+    'contact.apps.ContactConfig',
     'todo',
 ]
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'gestor_personal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
